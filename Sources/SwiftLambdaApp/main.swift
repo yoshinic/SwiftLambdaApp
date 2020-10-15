54141394a8961b357d4a178679d18dda59a461f0
yoshi <yoshinic@icloud.com>
2020-10-15 13:37:48 +0900

    追加：Lambda 関数の作成

--- Sources/SwiftLambdaApp/main.swift
+++ Sources/SwiftLambdaApp/main.swift
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import AWSLambdaRuntime
+
+struct Input: Codable {
+  let number: Double
+}
+
+struct Output: Codable {
+  let result: Double
+}
+
+Lambda.run { (context, input: Input, callback: @escaping (Result<Output, Error>) -> Void) in
+  callback(.success(Output(result: input.number * input.number)))
+}
